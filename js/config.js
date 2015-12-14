592ca110897e97ba9765c19c2d48faa9bcda545c
Muhammad Reza Irvanda <muzavan@gmail.com>
2015-12-14 14:08:35 +0700

    Tambah Test Case

--- js/config.js
+++ js/config.js
@@ -3,10 +3,10 @@ var config = {
         {
             type : 'title',
             content : {
-                background_image : "https://quotefancy.com/media/wallpaper/1600x900/1859-Bruce-Lee-Quote-Be-happy-but-never-satisfied.jpg",
-                tag_line : "Muzavan",
-                sub_tag_line : "Muhammad Reza Irvanda",
-                button : "About Me"
+                background_image : "https://quotefancy.com/media/wallpaper/1600x900/2110-Steve-Jobs-Quote-The-people-who-are-crazy-enough-to-think-they-can.jpg",
+                tag_line : "Muhammad Reza Irvanda",
+                sub_tag_line : "Programmer, Software Engineer, dan Pria Tampan",
+                button : "Tentang saya"
             }
         },
         {
@@ -16,99 +16,108 @@ var config = {
                 events: [
                     {
                         imageurl: "http://lorempixel.com/200/200/cats",
-                        title: "Makan Krupuk",
-                        subtitle: "Lomba 17 Agustus",
-                        description: "Makan krupuk di balai RW.",
+                        title: "ICPC",
+                        subtitle: "Indonesian Regional",
+                        description: "Programming skala dunia, meraih juara 3",
                         start: { year: 2014, month: 12, day: 1},
                         end : { year: 2015, month: 12, day: 1}
                     },
                     {
-                        imageurl: "http://lorempixel.com/200/201/cats",
-                        title: "Lomba Menyanyi",
-                        subtitle: "Hari Lebaran Kucing",
-                        description: "Menyanyi bersama Slipknot.",
-                        start: { year: 2014, month: 12, day: 1}
+                        imageurl: "http://lorempixel.com/200/200/cats",
+                        title: "ICPC",
+                        subtitle: "Indonesian Regional",
+                        description: "Programming skala dunia, meraih juara 3",
+                        start: { year: 2014, month: 12, day: 1},
+                        end : { year: 2015, month: 12, day: 1}
                     },
                     {
-                        imageurl: "http://lorempixel.com/200/202/cats",
-                        title: "Ngoding",
-                        subtitle: "Battle of Labtek V",
-                        description: "Ngoding sampai sakit kepala.",
-                        start: { year: 2014, month: 1, day: 1},
-                        end : { year: 2015, month: 9, day: 1}
-                    }
+                        imageurl: "http://lorempixel.com/200/200/cats",
+                        title: "ICPC",
+                        subtitle: "Indonesian Regional",
+                        description: "Programming skala dunia, meraih juara 3",
+                        start: { year: 2014, month: 12, day: 1},
+                        end : { year: 2015, month: 12, day: 1}
+                    },
+                    {
+                        imageurl: "http://lorempixel.com/200/200/cats",
+                        title: "ICPC",
+                        subtitle: "Indonesian Regional",
+                        description: "Programming skala dunia, meraih juara 3",
+                        start: { year: 2014, month: 12, day: 1},
+                        end : { year: 2015, month: 12, day: 1}
+                    },
+                    {
+                        imageurl: "http://lorempixel.com/200/200/cats",
+                        title: "ICPC",
+                        subtitle: "Indonesian Regional",
+                        description: "Programming skala dunia, meraih juara 3",
+                        start: { year: 2014, month: 12, day: 1},
+                        end : { year: 2015, month: 12, day: 1}
+                    },
+                    {
+                        imageurl: "http://lorempixel.com/200/200/cats",
+                        title: "ICPC",
+                        subtitle: "Indonesian Regional",
+                        description: "Programming skala dunia, meraih juara 3",
+                        start: { year: 2014, month: 12, day: 1},
+                        end : { year: 2015, month: 12, day: 1}
+                    },
                 ]
             }
         },
 		{
             type: 'portfolio',
             content: {
-                title: "Portfolio Humbaba",
-                description: "Contoh 1",
+                title: "Portfolio",
+                description: "Hasil pekerjaanku",
                 projects: [
                     {
-                        title: "Project 1",
-                        category: "Design",
-                        subtitle: "Index shard allocation",
-                        description: "This module provides per-index settings to control the allocation of shards to nodes.",
+                        title: "Tugas Besar IF4031",
+                        category: "PAT",
+                        subtitle: "Programming, Distributed System",
+                        description: "Merancang server yang mampu menangani ",
                         thumbnail_url: "http://lorempixel.com/200/201/cats",
                         image_url: "http://lorempixel.com/200/202/cats"
                     },
                     {
-                        title: "Project 2",
-                        category: "Repository system",
-                        subtitle: "Introducing a new repository design",
-                        description: " Hey there! We're rolling out a faster, more streamlined repository experience and would love to give you early access.",
-                        thumbnail_url: "http://lorempixel.com/200/203/cats",
-                        image_url: "http://lorempixel.com/200/204/cats"
+                        title: "Tugas Besar IF4031",
+                        category: "PAT",
+                        subtitle: "Programming, Distributed System",
+                        description: "Merancang server yang mampu menangani ",
+                        thumbnail_url: "http://lorempixel.com/200/201/cats",
+                        image_url: "http://lorempixel.com/200/202/cats"
                     },
                     {
-                        title: "Project 3",
-                        category: "Web Design",
-                        subtitle: "It used to be a readme",
-                        description: " Help people interested in this repository understand your project by adding a README. ",
-                        thumbnail_url: "http://lorempixel.com/200/205/cats",
-                        image_url: "http://lorempixel.com/200/206/cats"
+                        title: "Tugas Besar IF4031",
+                        category: "PAT",
+                        subtitle: "Programming, Distributed System",
+                        description: "Merancang server yang mampu menangani ",
+                        thumbnail_url: "http://lorempixel.com/200/201/cats",
+                        image_url: "http://lorempixel.com/200/202/cats"
                     },
                     {
-                        title: "Project 4",
-                        category: "Framework",
-                        subtitle: "Quick fox",
-                        description: "You've been idle for 3 minutes. Scroll, click or press any key to dismiss.",
-                        thumbnail_url: "http://lorempixel.com/200/207/cats",
-                        image_url: "http://lorempixel.com/200/208/cats"
+                        title: "Tugas Besar IF4031",
+                        category: "PAT",
+                        subtitle: "Programming, Distributed System",
+                        description: "Merancang server yang mampu menangani ",
+                        thumbnail_url: "http://lorempixel.com/200/201/cats",
+                        image_url: "http://lorempixel.com/200/202/cats"
                     },
                     {
-                        title: "Project 5",
-                        category: "Character Design",
-                        subtitle: "Manlyness is manly",
-                        description: "13+2-21*4/2.12 3x+7y*log(3x^k)/(e^n)=123w*x+56x^x+e^n+log(12^2x)+13!/e!+1/234.12^y",
-                        thumbnail_url: "http://lorempixel.com/200/209/cats",
-                        image_url: "http://lorempixel.com/200/210/cats"
+                        title: "Tugas Besar IF4031",
+                        category: "PAT",
+                        subtitle: "Programming, Distributed System",
+                        description: "Merancang server yang mampu menangani ",
+                        thumbnail_url: "http://lorempixel.com/200/201/cats",
+                        image_url: "http://lorempixel.com/200/202/cats"
                     },
                     {
-                        title: "Project 6",
-                        category: "Game",
-                        subtitle: "Best Game Ever",
-                        description: "This text is here just for placeholder until the game description is added",
-                        thumbnail_url: "http://lorempixel.com/200/211/cats",
-                        image_url: "http://lorempixel.com/200/212/cats"
-                    },
-                    {
-                        title: "Project 7",
-                        category: "Program",
-                        subtitle: "I don't know",
-                        description: "Why are you even looking at this here? It's not like there's any content here",
-                        thumbnail_url: "http://lorempixel.com/200/213/cats",
-                        image_url: "http://lorempixel.com/200/214/cats"
-                    },
-                    {
-                        title: "Project 8",
-                        category: "Web",
-                        subtitle: "Isitchristmasyet.com",
-                        description: "Best website ever, nothing even compare",
-                        thumbnail_url: "http://lorempixel.com/200/215/cats",
-                        image_url: "http://lorempixel.com/200/216/cats"
+                        title: "Tugas Besar IF4031",
+                        category: "PAT",
+                        subtitle: "Programming, Distributed System",
+                        description: "Merancang server yang mampu menangani ",
+                        thumbnail_url: "http://lorempixel.com/200/201/cats",
+                        image_url: "http://lorempixel.com/200/202/cats"
                     },
                 ]
             }
@@ -125,11 +134,15 @@ var config = {
                 profiles : [
                             {
                                 profile_type : "twitter",
-                                profile_username : "kucing_ganteng"
+                                profile_username : "muzavan"
                             },
                             {
                                 profile_type : "facebook",
-                                profile_username : "kucing_facebook"
+                                profile_username : "MuhammadRezaIrvanda"
+                            },
+                            {
+                                profile_type : "linkedin",
+                                profile_username : "muhammad-reza-irvanda-39"
                             }
                 ]
             }
---
+++ js/config_1.js
@@ -0,0 +1,151 @@
+var config = {
+    components: [
+        {
+            type : 'title',
+            content : {
+                background_image : "https://quotefancy.com/media/wallpaper/1600x900/2110-Steve-Jobs-Quote-The-people-who-are-crazy-enough-to-think-they-can.jpg",
+                tag_line : "Muhammad Reza Irvanda",
+                sub_tag_line : "Programmer, Software Engineer, dan Pria Tampan",
+                button : "Tentang saya"
+            }
+        },
+        {
+            type: 'experience',
+            content: {
+                title: "Pengalamanku",
+                events: [
+                    {
+                        imageurl: "http://lorempixel.com/200/200/cats",
+                        title: "ICPC",
+                        subtitle: "Indonesian Regional",
+                        description: "Programming skala dunia, meraih juara 3",
+                        start: { year: 2014, month: 12, day: 1},
+                        end : { year: 2015, month: 12, day: 1}
+                    },
+                    {
+                        imageurl: "http://lorempixel.com/200/200/cats",
+                        title: "ICPC",
+                        subtitle: "Indonesian Regional",
+                        description: "Programming skala dunia, meraih juara 3",
+                        start: { year: 2014, month: 12, day: 1},
+                        end : { year: 2015, month: 12, day: 1}
+                    },
+                    {
+                        imageurl: "http://lorempixel.com/200/200/cats",
+                        title: "ICPC",
+                        subtitle: "Indonesian Regional",
+                        description: "Programming skala dunia, meraih juara 3",
+                        start: { year: 2014, month: 12, day: 1},
+                        end : { year: 2015, month: 12, day: 1}
+                    },
+                    {
+                        imageurl: "http://lorempixel.com/200/200/cats",
+                        title: "ICPC",
+                        subtitle: "Indonesian Regional",
+                        description: "Programming skala dunia, meraih juara 3",
+                        start: { year: 2014, month: 12, day: 1},
+                        end : { year: 2015, month: 12, day: 1}
+                    },
+                    {
+                        imageurl: "http://lorempixel.com/200/200/cats",
+                        title: "ICPC",
+                        subtitle: "Indonesian Regional",
+                        description: "Programming skala dunia, meraih juara 3",
+                        start: { year: 2014, month: 12, day: 1},
+                        end : { year: 2015, month: 12, day: 1}
+                    },
+                    {
+                        imageurl: "http://lorempixel.com/200/200/cats",
+                        title: "ICPC",
+                        subtitle: "Indonesian Regional",
+                        description: "Programming skala dunia, meraih juara 3",
+                        start: { year: 2014, month: 12, day: 1},
+                        end : { year: 2015, month: 12, day: 1}
+                    },
+                ]
+            }
+        },
+		{
+            type: 'portfolio',
+            content: {
+                title: "Portfolio",
+                description: "Hasil pekerjaanku",
+                projects: [
+                    {
+                        title: "Tugas Besar IF4031",
+                        category: "PAT",
+                        subtitle: "Programming, Distributed System",
+                        description: "Merancang server yang mampu menangani ",
+                        thumbnail_url: "http://lorempixel.com/200/201/cats",
+                        image_url: "http://lorempixel.com/200/202/cats"
+                    },
+                    {
+                        title: "Tugas Besar IF4031",
+                        category: "PAT",
+                        subtitle: "Programming, Distributed System",
+                        description: "Merancang server yang mampu menangani ",
+                        thumbnail_url: "http://lorempixel.com/200/201/cats",
+                        image_url: "http://lorempixel.com/200/202/cats"
+                    },
+                    {
+                        title: "Tugas Besar IF4031",
+                        category: "PAT",
+                        subtitle: "Programming, Distributed System",
+                        description: "Merancang server yang mampu menangani ",
+                        thumbnail_url: "http://lorempixel.com/200/201/cats",
+                        image_url: "http://lorempixel.com/200/202/cats"
+                    },
+                    {
+                        title: "Tugas Besar IF4031",
+                        category: "PAT",
+                        subtitle: "Programming, Distributed System",
+                        description: "Merancang server yang mampu menangani ",
+                        thumbnail_url: "http://lorempixel.com/200/201/cats",
+                        image_url: "http://lorempixel.com/200/202/cats"
+                    },
+                    {
+                        title: "Tugas Besar IF4031",
+                        category: "PAT",
+                        subtitle: "Programming, Distributed System",
+                        description: "Merancang server yang mampu menangani ",
+                        thumbnail_url: "http://lorempixel.com/200/201/cats",
+                        image_url: "http://lorempixel.com/200/202/cats"
+                    },
+                    {
+                        title: "Tugas Besar IF4031",
+                        category: "PAT",
+                        subtitle: "Programming, Distributed System",
+                        description: "Merancang server yang mampu menangani ",
+                        thumbnail_url: "http://lorempixel.com/200/201/cats",
+                        image_url: "http://lorempixel.com/200/202/cats"
+                    },
+                ]
+            }
+        },
+        {
+            type: 'about',
+            content: {
+                photo : "http://lorempixel.com/200/200/cats",
+                name : "Muhammad Reza Irvanda",
+                address : "Jalan Ganesha No. 10",
+                telp_num : "081322975540",
+                email : "kucing@gmail.com",
+                description : "saya ganteng",
+                profiles : [
+                            {
+                                profile_type : "twitter",
+                                profile_username : "muzavan"
+                            },
+                            {
+                                profile_type : "facebook",
+                                profile_username : "MuhammadRezaIrvanda"
+                            },
+                            {
+                                profile_type : "linkedin",
+                                profile_username : "muhammad-reza-irvanda-39"
+                            }
+                ]
+            }
+        }
+    ]
+};
